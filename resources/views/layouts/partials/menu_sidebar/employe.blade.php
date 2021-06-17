d246740bbb16b9d9854c0fc72770c4c9cf0f7bec
joel goyo <joelgoyosaez@gmail.com>
2021-06-17 17:53:21 -0400

    iconos del menu lateral employes

--- resources/views/layouts/partials/menu_sidebar/employe.blade.php
+++ resources/views/layouts/partials/menu_sidebar/employe.blade.php
@@ -1,21 +1,21 @@
 <li class=" nav-item btn-menu">
-    <a href="{{ route('employes.home') }}"><i class="feather icon-home"></i>
-        <span class="menu-title" data-i18n="Email">Inicio</span></a>
+    <a href="{{ route('employes.home') }}"><img src="{{ asset('images/icons/home.png') }}" />
+        <span class="menu-title ml-1" data-i18n="Email">Inicio</span></a>
 </li>
 
 <li class=" nav-item">
-    <a href="{{ route('projects') }}"><i class="feather icon-clipboard"></i>
-        <span class="menu-title" data-i18n="Email">Proyectos</span></a>
+    <a href="{{ route('projects') }}"><img src="{{ asset('images/icons/maletin.png') }}" />
+        <span class="menu-title ml-1" data-i18n="Email">Proyectos</span></a>
 </li>
 
 <li class=" nav-item">
-    <a href="{{ route('bill') }}"><i class="feather icon-folder"></i>
-        <span class="menu-title" data-i18n="Email">Facturas</span></a>
+    <a href="{{ route('bill') }}"><img src="{{ asset('images/icons/mone.png') }}" />
+        <span class="menu-title ml-1" data-i18n="Email">Facturas</span></a>
 </li>
 
 <li class=" nav-item">
-    <a href="{{ route('holidays') }}"><i class="feather icon-help-circle"></i>
-        <span class="menu-title" data-i18n="Email">De interes</span></a>
+    <a href="{{ route('holidays') }}"><img src="{{ asset('images/icons/group_157.png') }}" />
+        <span class="menu-title ml-1" data-i18n="Email">De interes</span></a>
 </li>
 
 
@@ -40,8 +40,8 @@
 </li>-->
 
 <li class=" nav-item">
-    <a href="{{ route('profile') }}"><i class="feather icon-user"></i>
-        <span class="menu-title" data-i18n="Email">Perfil</span></a>
+    <a href="{{ route('profile') }}"><img src="{{ asset('images/icons/perfil.png') }}" />
+        <span class="menu-title ml-1" data-i18n="Email">Perfil</span></a>
 </li>
 
 
