8134e207b08ecec30737b36ba6e3350062e806f8
luisana <alexisjoseva95@gmail.com>
2021-06-28 15:41:13 -0400

    Migración tabla Payments

---
+++ database/migrations/2021_06_28_193725_create_payments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('bill_id');
+            $table->string('payment_method');
+            $table->double('amount');
+            $table->double('fee')->default(0);
+            $table->double('total');
+            $table->date('date');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('bill_id')->references('id')->on('bills');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
