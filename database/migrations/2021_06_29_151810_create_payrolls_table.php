c9feee7292b633a576dd87c808a640efe2522d18
luisana <alexisjoseva95@gmail.com>
2021-06-29 11:20:24 -0400

    Migración para la tabla payrolls

---
+++ database/migrations/2021_06_29_151810_create_payrolls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayrollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount');
+            $table->date('date');
+            $table->enum('status', [0, 1])->default(0)->comment('0 - Pendiente, 1 - Completada');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payrolls');
+    }
+}
