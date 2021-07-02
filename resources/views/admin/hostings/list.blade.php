a4c7396d42e4ef91d77527a5f747d0830008ddb4
joel goyo <joelgoyosaez@gmail.com>
2021-07-02 09:25:17 -0400

    reparar hosting y facturas

--- resources/views/admin/hostings/list.blade.php
+++ resources/views/admin/hostings/list.blade.php
@@ -30,17 +30,59 @@ class="vertical-layout vertical-menu-modern 2-columns navbar-floating footer-sta
             </div>
 
         </div>
+        <div class="content-body">
 
-<div class="content-body">
-    <div id="table-head">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title mb-1">Hosting</h3>
-                    <a href="{{ route('admin.hostings.create') }}" class="btn btn-primary mb-2 waves-effect waves-light"> Agregar nuevo proyecto</a>
+            <div id="table-head">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title mb-1">Hosting</h3>
+                            <a href="{{ route('admin.hostings.create') }}" class="btn btn-primary mb-2 waves-effect waves-light"> Agregar nuevo hosting</a>
+                        </div>
+
+                        <div class="card-content">
+                            <div class="table-responsive">
+                                <table class="table mb-0">
+                                    <thead class="thead-light">
+                                        <tr class="">
+                                            <th>DOMINIO</th>
+                                            <th>FECHA DE INICIO</th>
+                                            <th>CLIENTE</th>
+                                            <th>AÑOS</th>
+                                            <th>ACCIÓN</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($hostings as $hosting)
+                                        <tr>
+                                            <th scope="row">{{ $hosting->url }}</th>
+                                            <td>{{ date('d-m-Y', strtotime($hosting->start_date)) }}</td>
+                                            <td>
+                                                {{$hosting->user->name}}
+                                            </td>
+                                            <td>
+                                                {{$hosting->due_date}}
+                                            </td>
+                                            <td>
+                                                <a href="" class="mr-2" ><img id="bottom"src="{{asset('images/icons/Vector.png')}}" alt=""></a>
+                                                 <a href=""><img id="bottom"src="{{asset('images/icons/Group.png')}}" alt=""></a>
+                                            </td>
+                                        
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </div>
+
+
+
+
 @endsection
