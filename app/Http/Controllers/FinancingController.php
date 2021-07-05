0ce06de56a46b8495941e46db0a1b38c296c0920
joel goyo <joelgoyosaez@gmail.com>
2021-07-05 17:55:35 -0400

    pàginate de list projecst empleado data de finacing y show

--- app/Http/Controllers/FinancingController.php
+++ app/Http/Controllers/FinancingController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PayrollEmployee;
+use Illuminate\Support\Facades\Auth;
+
 class FinancingController extends Controller
 {   
     /** De Interés - Financiamiento
     *** Perfil: Empleado ***/
     public function list(){
-        return view('employee.financing'); 
+
+        $employes = PayrollEmployee::where('user_id', Auth::id())->paginate(10);
+
+        return view('employee.financing')->with(compact('employes')); 
     }
 }
