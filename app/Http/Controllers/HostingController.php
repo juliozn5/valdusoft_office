73d23576a1803becec9a2376eb99b925001bb3ac
Abraham-maker <imnoteverywhere12@gmail.com>
2021-10-13 12:12:29 -0400

    añadimos parte de la integracion del Cpanel

--- app/Http/Controllers/HostingController.php
+++ app/Http/Controllers/HostingController.php
@@ -8,84 +8,92 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+
 class HostingController extends Controller
 {
     /** Listado de Hostings
-    *** Perfil: Admin - Cliente ***/
-    public function list(){
-        if (Auth::user()->profile_id == 1){
+     *** Perfil: Admin - Cliente ***/
+    public function list()
+    {
+        if (Auth::user()->profile_id == 1) {
             $hostings = Hosting::paginate(10);
-            $client = User::all()->where ('profile_id', '2');
-            return view('admin.hostings.list')->with(compact('client','hostings'));
-        }else if (Auth::user()->profile_id == 2){
+            $client = User::all()->where('profile_id', '2');
+            return view('admin.hostings.list')->with(compact('client', 'hostings'));
+        } else if (Auth::user()->profile_id == 2) {
             $hostings = Hosting::where('user_id', '=', Auth::user()->id)->paginate(10);
             return view('client.hostings')
-            ->with('hostings', $hostings);   
+                ->with('hostings', $hostings);
         }
     }
 
-    function show(Request $request, $id){
-        $hosting = Hosting::where('id', '=', $id)
-                        ->first();
-        $client = User::all()->where ('profile_id', '2');
-
-        return view('admin.hostings.show')->with(compact('hosting','client'));
+    function show(Request $request, $id)
+    {
+        $hosting = Hosting::where('id', '=', $id)->first();
+        $client = User::all()->where('profile_id', '2');
+        return view('admin.hostings.show')->with(compact('hosting', 'client'));
     }
 
     /** Crear Nuevo Hosting
-    *** Perfil: Admin ***/
-    public function create(){
+     *** Perfil: Admin ***/
+    public function create()
+    {
         $clients = DB::table('users')
-        ->select('id', 'name', 'last_name')
-        ->where('profile_id', '=', 2)
-        ->orderBy('name', 'ASC')
-        ->get();
+            ->select('id', 'name', 'last_name')
+            ->where('profile_id', '=', 2)
+            ->orderBy('name', 'ASC')
+            ->get();
 
         return view('admin.hostings.create')->with(compact('clients'));
     }
 
     /** Guardar datos del Nuevo Hosting
-    *** Perfil: Admin ***/
-    public function store(Request $request){
+     *** Perfil: Admin ***/
+    public function store(Request $request)
+    {
 
 
         $fecha = new Carbon($request->create_date);
         $renewal_hosting = $fecha->addYears($request->years);
-        
-      Hosting::create([
+
+        Hosting::create([
             'user_id' => $request->user_id,
             'url' => $request->url,
             'create_date' => $request->create_date,
             'due_date' => $request->due_date,
-            'renewal_hosting'=> strtotime(date($renewal_hosting)),
+            'renewal_hosting' => strtotime(date($renewal_hosting)),
             'price' => $request->price,
             'renewal_price' => $request->renewal_price,
             'years' => $request->years,
+            'cpanel_url' =>$request->cpanel_url,
+            'cpanel_user' => $request->cpanel_user,
+            'cpanel_password' => $request->cpanel_password,
         ]);
 
 
-        return redirect()->route('admin.hostings.list')->with('message','Se creo el Hosting Exitosamente');
+        return redirect()->route('admin.hostings.list')->with('message', 'Se creo el Hosting Exitosamente');
     }
 
     /** Editar datos de un Hosting
-    *** Perfil: Admin ***/
-    public function edit($id){
+     *** Perfil: Admin ***/
+    public function edit($id)
+    {
         $hosting = Hosting::find($id);
         dd($hosting);
 
         return view('admin.hostings.edit')
-           ->with('hosting', $hosting);  
+            ->with('hosting', $hosting);
     }
 
     /** Guardar datos modificados de un Hosting
-    *** Perfil: Admin ***/
-    public function update(Request $request){
+     *** Perfil: Admin ***/
+    public function update(Request $request)
+    {
 
         $hosting = Hosting::find($request->hosting_id);
-        
-        $fields = [     ];
 
-        $msj = [       ];
+        $fields = [];
+
+        $msj = [];
 
         $this->validate($request, $fields, $msj);
         $hosting->update($request->all());
@@ -96,38 +104,42 @@ class HostingController extends Controller
         $hosting->due_date = $fecha->addYears($request->date_end);
         $hosting->years = $request->date_end;
         $hosting->price = $request->price;
-        
-        
+        $hosting->cpanel_url = $request->cpanel_url;
+        $hosting->cpanel_user = $request->cpanel_user;
+        $hosting->cpanel_password = $request->cpanel_password;
+
+
         // $hosting->renewal_price = $request->renewal_price;
         // $hosting->renewal_hosting = $request->renewal_hosting;
 
         $hosting->save();
 
-        return redirect()->back()->with('message','Se actualizo el Hosting Exitosamente');
+        return redirect()->back()->with('message', 'Se actualizo el Hosting Exitosamente');
     }
 
-    public function renewal(Request $request){
+    public function renewal(Request $request)
+    {
         $hosting = Hosting::find($request->hosting_id);
-        
+
         $hosting->update($request->all());
 
         $hosting->renewal_price = $request->renewal_price;
         $hosting->renewal_hosting = $request->renewal_hosting;
 
         $hosting->save();
-        
-        return redirect()->back()->with('message','Se actualizo el Hosting Exitosamente');
 
+        return redirect()->back()->with('message', 'Se actualizo el Hosting Exitosamente');
     }
 
     /** Eliminar un Hosting
-    *** Perfil: Admin ***/
-    public function delete($id){
+     *** Perfil: Admin ***/
+    public function delete($id)
+    {
 
         $hosting = Hosting::find($id);
-    
+
         $hosting->delete();
-      
-        return redirect()->route('admin.hostings.list')->with('message','Se elimino el Hosting'.' '.$hosting->client.' '.'Exitosamente');
+
+        return redirect()->route('admin.hostings.list')->with('message', 'Se elimino el Hosting' . ' ' . $hosting->client . ' ' . 'Exitosamente');
     }
 }
