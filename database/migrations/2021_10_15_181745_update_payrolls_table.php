e134b22d53801d9316c5ff278e475fc4ea701e9b
Luisana Marin <luisanaelenamarin@gmail.com>
2021-10-15 16:09:55 -0400

    Creacion y actualizacion de nómina

---
+++ database/migrations/2021_10_15_181745_update_payrolls_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePayrollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payrolls', function (Blueprint $table) {
+            $table->dropColumn(['date']);
+            $table->date('start_date');
+            $table->date('dead_line');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
