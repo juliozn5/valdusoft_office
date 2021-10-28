01c15bef0611981608e672aee11e671610911fee
Abraham-maker <imnoteverywhere12@gmail.com>
2021-10-28 08:05:42 -0400

    home employee

--- resources/views/employee/home.blade.php
+++ resources/views/employee/home.blade.php
@@ -19,13 +19,16 @@ class="vertical-layout vertical-menu-modern 2-columns navbar-floating footer-sta
                         <div class="row">
                             @foreach ($proyects_user as $item)
                             <a href="{{ route('employee.projects.show', [$item->slug, $item->id]) }}" title="Ir al proyecto {{$item->name}}">
-                                <div class="container-fluid col-md-2 col-sm-1 mb-2 rounded" id="recomiendo">
-                                    <img src="{{ asset('uploads/images/projects/'.$item->logo) }}" style="width:100%; border-radius: 7px 7px 0 0;">
-                                    <div class="pr-1 h4 pb-2 text-center text-white" id="shadow">
-                                        <div style="position: relative;top: 14px;"> {{$item->name}}</div>
+                             
+                                <div class="card container-fluid" style="width: 18rem;" id="recomiendo">
+                                    <img src="{{ asset('uploads/images/projects/'.$item->logo) }}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <div class=" h4 pb-2 " id="shadow">
+                                            <div class="ml-1  text-white" style="position: relative;top: 14px;"> {{$item->name}}</div><a>
+                                        </div>
                                     </div>
-                            </a>
-                            @endforeach
+                                </div>
+                                @endforeach
                         </div>
 
                     </div>
@@ -33,50 +36,50 @@ class="vertical-layout vertical-menu-modern 2-columns navbar-floating footer-sta
             </div>
         </div>
     </div>
-        <section id="dashboard-analytics">
-            <div class="row">
-                <div class="col-md-4 col-sm-12">
-                    <div class="card p-2">
-                        <div class="">
-                            <div class="card-body">
-                                <img src="{{ asset('images/figma/group_154.png') }}" class="float-right pl-2" width="120" height="120" alt="">
-                                <h6 class="pt-2">Ultima factura de la quincena</h6>
-                                @if ($lastBill != NULL)
-                                <a href="{{ route('employee.bills.download', $lastBill->id) }}" class="btn btn-primary mt-1 pb-1" target="_blank" @if (is_null($lastBill)) disabled @endif><b>Descargar</b></a>
-                                @else
-                                <a href="#" class="btn btn-primary mt-1 pb-1"><b>Sin facturas</b></a>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="card p-2">
-                        <div class="">
-                            <div class="card-body">
-                                <img src="{{ asset('images/figma/dolar.png') }}" class="float-right pl-2" width="120" height="120" alt="">
-                                <h5 class="pt-2">Valor de la hora de trabajo</h5>
-                                <br>
-                                <p class="h4" id="holidays-date">$ @isset($user->price_per_hour) {{number_format($user->price_per_hour, 2, ',', '.')}} @else 0,00 @endisset</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-sm-12">
-                    <div class="card p-2">
-                        <div class="">
-                            <div class="card-body">
-                                <img src="{{ asset('images/svg/ilustracion_clientes.svg') }}" class="float-right pl-2" width="120" height="120" alt="">
-                                <h5 class="pt-2">Proximas <br> Vacaciones</h5>
-                                <br>
-                                <p class="h5 " id="holidays-date"><i class="far fa-calendar icon-big mr-1"></i>{{$fechaUser->toFormattedDateString()}}</p>
-                            </div>
+    <section id="dashboard-analytics">
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <div class="card p-2">
+                    <div class="">
+                        <div class="card-body">
+                            <img src="{{ asset('images/figma/group_154.png') }}" class="float-right pl-2" width="120" height="120" alt="">
+                            <h6 class="pt-2">Ultima factura de la quincena</h6>
+                            @if ($lastBill != NULL)
+                            <a href="{{ route('employee.bills.download', $lastBill->id) }}" class="btn btn-primary mt-1 pb-1" target="_blank" @if (is_null($lastBill)) disabled @endif><b>Descargar</b></a>
+                            @else
+                            <a href="#" class="btn btn-primary mt-1 pb-1"><b>Sin facturas</b></a>
+                            @endif
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="card p-2">
+                    <div class="">
+                        <div class="card-body">
+                            <img src="{{ asset('images/figma/dolar.png') }}" class="float-right pl-2" width="120" height="120" alt="">
+                            <h5 class="pt-2">Valor de la hora de trabajo</h5>
+                            <br>
+                            <p class="h4" id="holidays-date">$ @isset($user->price_per_hour) {{number_format($user->price_per_hour, 2, ',', '.')}} @else 0,00 @endisset</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-12">
+                <div class="card p-2">
+                    <div class="">
+                        <div class="card-body">
+                            <img src="{{ asset('images/svg/ilustracion_clientes.svg') }}" class="float-right pl-2" width="120" height="120" alt="">
+                            <h5 class="pt-2">Proximas <br> Vacaciones</h5>
+                            <br>
+                            <p class="h5 " id="holidays-date"><i class="far fa-calendar icon-big mr-1"></i>{{$fechaUser->toFormattedDateString()}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 </div>
 @endsection
