89fcc2701d5e6ef6022fcde08448a42390994d64
Abraham-maker <imnoteverywhere12@gmail.com>
2021-10-29 04:18:49 -0400

    añadimos las seed de los paises

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        
         // \App\Models\User::factory(10)->create();
-        // $this->call(UserSeeder::class);
+         $this->call(ContriesSedders::class);
         
     }
 }
